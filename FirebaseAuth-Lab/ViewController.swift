f9f410a6f4eefc48dc44a2e8b61d2b60a896705c
Yuliia Engman <yuliia.engman@gmail.com>
2020-03-11 12:53:39 -0400

    Initial Commit

---
+++ FirebaseAuth-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseAuth-Lab
+//
+//  Created by Yuliia Engman on 3/11/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
